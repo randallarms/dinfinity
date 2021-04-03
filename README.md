d7c38aa71b39cd9449d4bbb6c04dd3bf6dd0aa11
Randall Arms <contact.randallArms@gmail.com>
2021-04-03 16:34:25 -0700

    v3 | Added looping and better checks
    * Added a while loop to keep the program rolling as long as you want it to.
    * Added better checks to accommodate the looping.

--- README.md
+++ README.md
@@ -9,8 +9,7 @@ When prompted, type in the desired number of side on each die.
 
 You will then be prompted to enter the number of dice to roll.
 > Note: If the numbers are large then a confirmation will be requested. Type "yes" and hit enter to continue.
+> WARNING: The program will attempt to iterate through every roll one by one. Some incredibly high values may cause the program to take a very long time to finish!
 
 The program should then iterate through each roll up to the number of rolls selected, inclusive.
-> Tip: Rolling the traditional six-sided dice (d6) will output an ASCII image of the roll results as well.
-
-> Note: More options will be added soon, such as for calculating average/mean rolls and more.
+> Tip: Rolling the traditional two six-sided dice (2d6) will output an ASCII image of the roll results as well.
--- dinfinity.py
+++ dinfinity.py
@@ -12,20 +12,14 @@ print("dInfinity")
 print("=========")
 print("Roll the dice!")
 
-# Rolls array
-rolls = []
-
 # Checks that the input n is a positive integer and returns an obj of int type
 def int_check(n):
-    if (int(n) < 1):
-        print("\nInput number must be greater than 0. ")
-        exit()
     try:
-        return int(n)
+        return True, int(n)
     except ValueError:
-        print("\nInput number must be an integer. ")
-        exit()
+        return False, 0
     
+
 # Create the roll ASCII image
 def d_img(rolls):
     roller = {
@@ -48,39 +42,66 @@ def d_img(rolls):
     for dice in range(len(rolls)):
         d_img_str += "---------" + "   "
     d_img_str += "\n"
+    
     return d_img_str
 
-# Input number of sides on each die
-print("\nHow many sides on each die? ")
-s = input("> ");
 
-s_int = int_check(s)
+# Exit phrase array & check var
+exiters = ["exit", "quit", "leave", "n", "no", "negative"]
+r_in = "y"
 
-# Input number of dice to roll
-print("\nHow many dice to roll? ")
-d = input("> ");
-
-d_int = int_check(d)
-
-# Check for large numbers and confirm
-if s_int > 100 or d_int > 100:
-    print("\nThis roll is complex and may take a long time to finish. Type yes to confirm: ")
-    confirm = input("> ");
-    if confirm != "yes":
-        print("\nRoll canceled. ")
-        exit()
-
-# Iterate through the integers and print results
-print("\nRolling... \n")
-total = 0
-for roll in range(1, d_int+1):
-    result = randint(1, s_int)
-    rolls.append(result)
-    total += result
-    print("Roll #" + str(roll) + ": " + str(result))
-
-print("\nTOTAL: " + str(total))
+# Run the prompt and print the results
+while r_in not in exiters:
+    rolls = []
     
-# If rolling d6, then add ASCII image as well
-if s_int == 6:
-    print(d_img(rolls))
+    # Input number of sides on each die
+    print("\nHow many sides on each die? ")
+    s_in = input("> ");
+
+    # Check if int and convert
+    s_check, s_int = int_check(s_in)
+
+    # Input number of dice to roll
+    print("\nHow many dice to roll? ")
+    d_in = input("> ");
+
+    # Check if int and convert
+    d_check, d_int = int_check(d_in)
+    
+    # Check the input
+    if d_check is False or s_check is False:
+        print("\nInput numbers must be integers. ")
+    elif d_int < 1 or s_int < 1:
+        print("\nInput numbers must be greater than 0. ")       
+    else:
+        # Check for large numbers and confirm
+        if s_int > 100 or d_int > 100:
+            print("\nThis roll is complex and may take a long time to finish. Type yes to confirm: ")
+            confirm = input("> ");
+            if confirm != "yes":
+                print("\nRoll canceled. ")
+                exit()
+                
+        # Iterate through the integers and print results
+        print("\nRolling... \n")
+        total = 0
+        for roll in range(1, d_int+1):
+            result = randint(1, s_int)
+            rolls.append(result)
+            total += result
+            print("Roll #" + str(roll) + ": " + str(result))
+
+        print("\nTOTAL: " + str(total))
+            
+        # If rolling d6, then add ASCII image as well
+        if s_int == 6:
+            print(d_img(rolls))
+            
+    print("\nWould you like to roll again? (y/n) ")
+    r_in = input("> ")
+
+
+#Salutations and exit
+print("\nThank you for using dInfinity. Goodbye! \n")
+exit()
+        
